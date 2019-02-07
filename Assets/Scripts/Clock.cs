bf4d5ceba062724aa1da77399f0aca01d8a4f495
Danyow <i.zuucol@gmail.com>
2019-02-07 20:22:14 +0800

    连续钟表

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -6,17 +6,36 @@ using UnityEngine;
 public class Clock : MonoBehaviour
 {
 
-    // 每小时度数
     const float 
+    // 每小时度数
         degreesPerHour = 30f, 
-        degreesPerMinute = 6f, 
-        degreesPerSecond = 6f; 
     // 每分钟度数
-
+        degreesPerMinute = 6f, 
     // 每秒钟度数
+        degreesPerSecond = 6f; 
+    public bool continuous;
+
     public Transform hoursTransform, minutesTransform, secondsTransform;
 
+
     private void Update() {
+        if (continuous) {
+            UpdateContinuous();
+        } else {
+            UpdateDiscrete();
+        }
+    }
+
+    // 连续的
+    private void UpdateContinuous() {
+        TimeSpan nowTime = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation   = Quaternion.Euler(0f, (float)nowTime.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float)nowTime.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, (float)nowTime.TotalSeconds * degreesPerSecond, 0f);
+    }
+
+    // 离散的
+    private void UpdateDiscrete() {
         DateTime nowTime = DateTime.Now;
         hoursTransform.localRotation   = Quaternion.Euler(0f, nowTime.Hour * degreesPerHour, 0f);
         minutesTransform.localRotation = Quaternion.Euler(0f, nowTime.Minute * degreesPerMinute, 0f);
