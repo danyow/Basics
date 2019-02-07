4326dc0c0096bbdbd0a5319267e79ec1aac3b991
Danyow <i.zuucol@gmail.com>
2019-02-07 18:44:58 +0800

    可以动的钟

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+
+    // 每小时度数
+    const float 
+        degreesPerHour = 30f, 
+        degreesPerMinute = 6f, 
+        degreesPerSecond = 6f; 
+    // 每分钟度数
+
+    // 每秒钟度数
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+
+    private void Update() {
+        DateTime nowTime = DateTime.Now;
+        hoursTransform.localRotation   = Quaternion.Euler(0f, nowTime.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, nowTime.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, nowTime.Second * degreesPerSecond, 0f);
+    }
+}
