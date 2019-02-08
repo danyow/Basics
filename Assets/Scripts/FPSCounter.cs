077d0dba21c44db8384b02a17f836d4ba6ab1b50
Danyow <i.zuucol@gmail.com>
2019-02-08 10:17:05 +0800

    FPS的显示完成

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int AverageFPS { get; private set;}
+    public int HighestFPS { get; private set; }
+    public int LowestFPS { get; private set; }
+    public int frameRange = 60;
+    int[] fpsBuffer;
+    int fpsBufferIndex;
+
+    private void Update() {
+        if (fpsBuffer == null || fpsBuffer.Length != frameRange) {
+            InitializeBuffer();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+    }
+
+    void InitializeBuffer() {
+        if (frameRange <= 0) {
+            frameRange = 1;
+        }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+
+    }
+
+    void UpdateBuffer() {
+        // 1 除以当前帧时间增量(deltaTime)就是每秒的帧数
+        // 但是这个时间增量不是处理最后一帧的时间 它呗当前时间的尺度所压缩 除非这个时间尺度一直为1 不然就是错误的 所以采用未被压缩的增量
+        fpsBuffer[fpsBufferIndex ++] = (int)(1f / Time.unscaledDeltaTime);
+        if (fpsBufferIndex >= frameRange) {
+            fpsBufferIndex = 0;
+        }
+    } 
+
+    void CalculateFPS() {
+        int sum = 0;
+        int highest = 0;
+        int lowest = int.MaxValue;
+        for (int i = 0; i < frameRange; i++) {
+            int fps = fpsBuffer[i];
+            sum += fps;
+            if (fps > highest) {
+                highest = fps;
+            }
+            if (fps < lowest) {
+                lowest = fps;
+            }
+        }
+        AverageFPS = sum / frameRange;
+        HighestFPS = highest;
+        LowestFPS = lowest;
+    }
+}
---
+++ Assets/Scripts/Nucleon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+    // 吸引力
+    public float attractionForce;
+    Rigidbody body;
+
+
+    private void Awake() {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+}
